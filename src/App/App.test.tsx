5a6e0b1cd3f067906d7c97567fea2aab8e387dbf
alexpeev9 <alexpeev9@abv.bg>
2022-08-19 15:46:50 +0300

    :lipstick: Style Design

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -2,8 +2,8 @@ import { screen, render } from '@testing-library/react'
 import App from '.'
 
 describe('App Component', () => {
-  test("Check if heading has a value - 'Template'", () => {
+  test('Check if heading has correct value', () => {
     render(<App />)
-    screen.getByText('Template')
+    screen.getByText('React Webpack 5 Template')
   })
 })
